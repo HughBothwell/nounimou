df6870cf20c12c09c37bb3f3c0a7bf392f45bb32
Hugh Bothwell <hugh_bothwell@hotmail.com>
2019-03-05 12:52:18 -0500

    Add files via upload

--- zagreb.py
+++ zagreb.py
@@ -1,6 +1,29 @@
 # Skyfield working stuff
 # https://rhodesmill.org/skyfield/files.html
+from math import *
 from skyfield import almanac, api
+
+MOON_DIA      =      3475000    # meters
+
+def moon_angle(dist):
+    """
+    Given a viewing distance in meters,
+    return the Moon's subtended angle in radians
+    """
+    return 2. * asin(0.5 * MOON_DIA / dist)
+    
+def rad_to_degminsec(angle, fmt="{deg:01d}deg {min:01d}' {sec:0.1f}\""):
+    """
+    Given an angle in radians,
+      return it as a degrees arcmin arcsec string
+      where degrees is in [0 .. 360)
+    """
+    angle = degrees(angle) % 360.
+    deg = int(angle)
+    rem = 60. * (angle - deg)
+    min_ = int(rem)
+    sec = 60. * (rem - min_)
+    return fmt.format(deg=deg, min=min_, sec=sec)
 
 # load ephemeris data
 load = api.Loader("d:/skyfield-data")   # set up data dir
@@ -17,8 +40,10 @@ ts = load.timescale()
 photo_time = ts.utc(2019, 2, 20, 4, 13)          # UTC time of Lexington photo
 
 # find alt/az of Moon
-lex_moon = (earth + lex).at(photo_time).observe(moon).apparent()
-print("Lexington:", lex_moon.altaz("standard"))
+lex_moon = (earth + lex).at(photo_time).observe(moon)
+print("Lexington:", lex_moon.apparent().altaz("standard"))
+print("Size:", rad_to_degminsec(moon_angle(lex_moon.distance().m)))
 
-zag_moon = (earth + zag).at(photo_time).observe(moon).apparent()
-print("Zagreb:", zag_moon.altaz("standard"))
+zag_moon = (earth + zag).at(photo_time).observe(moon)
+print("Zagreb:", zag_moon.apparent().altaz("standard"))
+print("Size:", rad_to_degminsec(moon_angle(zag_moon.distance().m)))
